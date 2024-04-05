d0e110364335eea93ad6248d42834c9891876310
D.S. Webster <dswebs.me@gmail.com>
2024-04-05 08:20:10 -0400

    Resolve notice

--- wp-documentation.php
+++ wp-documentation.php
@@ -72,7 +72,7 @@ function wpd_restrict_frontend_access( $query ) {
 	// Protect the documentation post type.
 	if (
 		// This is the main query.
-		is_main_query() &&
+		$query->is_main_query() &&
 
 		// The post_type query is set.
 		isset( $query->query['post_type'] ) &&
